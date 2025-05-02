490ecff9dc1d04970afd7298c216d73df81f0c2b
Spencer Perkins <95900100+SpecialThing44@users.noreply.github.com>
2025-05-02 12:31:09 -0400

    TransactionalContextWrapper

--- community/cypher/interpreted-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/interpreted/TransactionalContextWrapper.scala
+++ community/cypher/interpreted-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/interpreted/TransactionalContextWrapper.scala
@@ -27,7 +27,6 @@ import org.neo4j.internal.kernel.api.security.SecurityContext
 import org.neo4j.kernel.GraphDatabaseQueryService
 import org.neo4j.kernel.api.KernelTransaction.Revertable
 import org.neo4j.kernel.api.dbms.DbmsOperations
-import org.neo4j.kernel.api.query.CompilerInfo
 import org.neo4j.kernel.api.txstate.TxStateHolder
 import org.neo4j.kernel.api.{KernelTransaction, ResourceTracker, Statement}
 import org.neo4j.kernel.impl.api.SchemaStateKey
